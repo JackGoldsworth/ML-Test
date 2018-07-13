2364c479c0c1d1bb044ef8c1c7ce7b348d39ec15
JTGhawk137 <jackgoldsworth@gmail.com>
2018-07-13 03:11:43 -0500

    matplot & saving data

--- stockregression/stock_regression.py
+++ stockregression/stock_regression.py
@@ -1,11 +1,14 @@
+import datetime
 import math
 import quandl
+import matplotlib.pyplot as plt
 import numpy as np
-from sklearn import preprocessing, cross_validation
+from sklearn import preprocessing, model_selection
 from sklearn.linear_model import LinearRegression
+import pickle
 
 
-# Sentdex ML tutorial p.2 - 4
+# Sentdex ML tutorial p.2 - 6
 class StockRegression:
     # Getting the data from Quandl
     data = quandl.get("WIKI/GOOGL")
@@ -22,17 +25,45 @@ class StockRegression:
     data.fillna(-99999, inplace=True)
 
     # The price label looking forward
-    forecast_out = int(math.ceil(0.01*len(data)))
+    forecast_out = int(math.ceil(0.01 * len(data)))
 
     data["label"] = data[forecast_col].shift(-forecast_out)
+
+    x = np.array(data.drop(['label'], 1))
+    x = preprocessing.scale(x)
+    x_lately = x[-forecast_out:]
+    x = x[:-forecast_out:]
     data.dropna(inplace=True)
 
-    x = np.array(data.drop(["label"], 1))
     y = np.array(data["label"])
-    x = preprocessing.scale(x)
 
-    x_train, x_test, y_train, y_test = cross_validation.train_test_split(x, y, test_size=.02)
+    x_train, x_test, y_train, y_test = model_selection.train_test_split(x, y, test_size=.02)
     clf = LinearRegression()
     clf.fit(x_train, y_train)
+    with open("data.pickle", "wb") as f:
+        pickle.dump(clf, f)
+
+    pickle_in = open("data.pickle", "rb")
+    clf = pickle.load(pickle_in)
     accuracy = clf.score(x_test, y_test)
-    print(accuracy)
+
+    forecast_set = clf.predict(x_lately)
+
+    data['Forecast'] = np.nan
+
+    last_date = data.iloc[-1].name
+    last_unix = last_date.timestamp()
+    one_day = 86400
+    next_unix = last_unix + one_day
+
+    for i in forecast_set:
+        next_date = datetime.datetime.fromtimestamp(next_unix)
+        next_unix += 86400
+        data.loc[next_date] = [np.nan for _ in range(len(data.columns) - 1)] + [i]
+
+    data['Adj. Close'].plot()
+    data['Forecast'].plot()
+    plt.legend(loc=4)
+    plt.xlabel('Date')
+    plt.ylabel('Price')
+    plt.show()
