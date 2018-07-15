ead419d5c317bccea536f49ea46f719a427c6d61
JTGhawk137 <jackgoldsworth@gmail.com>
2018-07-15 03:08:51 -0500

    tada

--- customregression/custom_regression.py
+++ customregression/custom_regression.py
@@ -1,22 +1,26 @@
 import matplotlib.pyplot as plt
-import numpy as np
 from matplotlib import style
 from customregression.regression_util import RegressionUtil
 
 
+# Uses fake data for now, and doesn't exactly build off of the error,
+# but for now I'm just following tutorials so...
+# Sentdex ML tutorial p.7 - 12
 class CustomRegression:
 
     def main(self):
         style.use("fivethirtyeight")
-        x = np.array([1, 2, 3, 4, 5, 6], dtype=np.float64)
-        y = np.array([5, 4, 2, 1, 6, 7], dtype=np.float64)
 
         util = RegressionUtil()
+        x, y = util.create_dataset(40, 40, 2, correlation='pos')
         m = util.slope(x, y)
         b = util.intercept(x, y)
 
         regression_line = [(m * x) + b for x in x]
 
-        plt.scatter(x, y)
-        plt.plot(x, regression_line)
+        r_squared = util.cod(y, regression_line)
+        print(r_squared)
+
+        plt.scatter(x, y, label="data")
+        plt.plot(x, regression_line, label="regression line")
         plt.show()
--- customregression/regression_util.py
+++ customregression/regression_util.py
@@ -1,11 +1,37 @@
 from statistics import mean
+import random
+import numpy as np
 
 
 class RegressionUtil:
-
 
     def slope(self, x, y):
         return ((mean(x) * mean(y)) - mean(x * y)) / ((mean(x) * mean(x)) - mean(x ** 2))
 
     def intercept(self, x, y):
         return mean(y) - (self.slope(x, y) * mean(x))
+
+    def squared_error(self, y_point, y_line):
+        return sum((y_point - y_line) ** 2)
+
+    # Coefficient of Determination
+    def cod(self, y_point, y_line):
+        y_mean_line = [mean(y_point) for y in y_point]
+        squared_error = self.squared_error(y_point, y_line)
+        squared_error_mean = self.squared_error(y_point, y_mean_line)
+        return 1 - squared_error / squared_error_mean
+
+    # Just creates a fake data set.
+    def create_dataset(self, hm, variance, step=2, correlation=False):
+        val = 1
+        ys = []
+        for i in range(hm):
+            y = val + random.randrange(-variance, variance)
+            ys.append(y)
+            if correlation and correlation == 'pos':
+                val += step
+            elif correlation and correlation == 'neg':
+                val -= step
+
+        xs = [i for i in range(len(ys))]
+        return np.array(xs, dtype=np.float64), np.array(ys, dtype=np.float64)
